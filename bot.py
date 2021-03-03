f86375a8d805989a584c76593b83aad926eed988
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2021-03-03 17:22:58 -0300

    Number for csv file

--- bot.py
+++ bot.py
@@ -1,27 +1,38 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
+import csv
 
 
 class WhatsappBot():
+
     def __init__(self):
         # Parte 1 - A mensagem que você quer enviar
-        self.mensagem = "Colocando em mais de um grupo"
+        self.mensagem = ":)"
         # Parte 2 - Nome dos grupos ou pessoas a quem você deseja enviar a mensagem
         # self.grupos_ou_pessoas = ["Coisas minhas"]
+
         options = webdriver.ChromeOptions()
-        options.add_argument('lang=pt-br')
+        options.add_argument('user-data-dir=/mnt/c/Users/sahud/AppData/Local/Google/Chrome/User Data')
         self.driver = webdriver.Chrome(
             executable_path=r"./chromedriver.exe", chrome_options=options)
 
-    def EnviarMensagens(self,numero_pessoa ):
-        self.grupos_ou_pessoas = [numero_pessoa]
-        self.driver.get('https://web.whatsapp.com/')
+    def EnviarMensagens(self,resultados):
+        self.grupos_ou_pessoas = resultados
+        # self.grupos_ou_pessoas = resultados
+
+        self.driver.get('https://api.whatsapp.com')
+        # self.driver.get('https://web.whatsapp.com/')
         time.sleep(15)
         for grupo_ou_pessoa in self.grupos_ou_pessoas:
-            campo_grupo = self.driver.find_element_by_xpath(
-                f"//span[@title='{grupo_ou_pessoa}']")
+            
+            self.driver.get('https://api.whatsapp.com/send?phone=' + grupo_ou_pessoa)
             time.sleep(3)
-            campo_grupo.click()
+            alert_obj = self.driver.switch_to.alert
+            time.sleep(3)
+            alert_obj.accept()
+
+            time.sleep(15)
             chat_box = self.driver.find_element_by_class_name('_2A8P4')
             time.sleep(3)
             chat_box.click()
@@ -32,9 +43,12 @@ class WhatsappBot():
             botao_enviar.click()
             time.sleep(5)
 
-        
-print("Digite o numero")
-nome = input()
+arquivo = open('numeros.csv')
+
+linhas = csv.reader(arquivo)
+
+for linha in linhas:
+    print(linha)
 
 bot = WhatsappBot()
-bot.EnviarMensagens(nome)
+bot.EnviarMensagens(linha)
